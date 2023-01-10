7f7268afcb04f39bd304b84c94445d98d585e23b
Gaurav <118818573+gauravsrivastav189@users.noreply.github.com>
2023-01-11 05:11:50 +0530

    Update urls.py

--- learnpihi/members/urls.py
+++ learnpihi/members/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.index, name="index"),
-    path('add', views.add, name="add"),
+    path('add/', views.add, name="add"),
     path('add/addcomment/', views.addcomment, name="addcomment"),
 ]
