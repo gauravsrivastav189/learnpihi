22e66c5178c003769558f8075d516f7c5e192d8d
Gaurav <118818573+gauravsrivastav189@users.noreply.github.com>
2023-01-11 05:08:09 +0530

    Update views.py

--- learnpihi/members/views.py
+++ learnpihi/members/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Members
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
 def index(request):
 	mymembers=Members.objects.all().values()
-	output=''
-	for x in mymembers:
-		output+=x[course_name]
 	template=loader.get_template("index.html")
 	context={
 	"mymembers":mymembers,
@@ -23,9 +19,9 @@ def add(request):
 	return HttpResponse(template.render({},request))
 
 def addcomment(request):
-	x=request.POST['course_name']
-	y=request.POST['star_rating']
-	z=request.POST['comments']
+	x=request.POST.get('course',False)
+	y=request.POST.get('star',False)
+	z=request.POST.get('cmt',False)
 	member=Members(course_name=x,star_rating=y,comments=z)
 	member.save()
 	return HttpResponseRedirect(reverse("index"))
